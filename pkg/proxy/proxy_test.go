53449f8a2a1eeb7f62cde76d8addcbe7179b627b
Marc Boorshtein <marc.boorshtein@tremolosecurity.com>
2026-02-11 09:23:39 -0500

    update dependencies, fix mocks

--- pkg/proxy/proxy_test.go
+++ pkg/proxy/proxy_test.go
@@ -14,7 +14,7 @@ import (
 	"strings"
 	"testing"
 
-	"github.com/golang/mock/gomock"
+	"go.uber.org/mock/gomock"
 	"k8s.io/apiserver/pkg/authentication/authenticator"
 	"k8s.io/apiserver/pkg/authentication/request/bearertoken"
 	"k8s.io/apiserver/pkg/authentication/user"
